b2597d6f66f21f6f7639949ea8a95203743e4c69
Oleh <name@surname.com>
2021-08-11 14:49:15 +0300

    Move routes to separate file and add public folder

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import express, { Response } from "express";
 import passport from "passport";
 import cors from "cors";
 import { initAuthStrategies } from "./auth/initStrategies";
-import { authUtils } from "./auth/utils";
 import cookieParser from "cookie-parser";
+import { apiRouter } from "./routes";
 
 initAuthStrategies.initGoogle();
 initAuthStrategies.initJwt();
@@ -15,46 +15,10 @@ app.use(cookieParser());
 app.use(cors());
 app.use(passport.initialize());
 
-app.get("/api/auth/google", (req,res,next) => {
-        passport.authenticate("google", {
-            session: false,
-            scope: ["email", "profile"],
-// TODO add 'state' if necessary here
-            state: ""
-        })(req, res, next);
-    }
-);
-
-app.get("/api/auth/google/callback",
-    passport.authenticate(
-        "google",
-        {
-            session: false,
-            failureRedirect: "/auth/failure"
-        }
-    ),
-    (req, res: Response, next) => {
-        const token: string = authUtils.generateUserToken(req, res);
-        // This is html wrapper to be opened in separate window
-        let responseHTML = '<html><head><title>Main</title></head><body></body><script>res = %value%; window.opener.postMessage(res, "*");window.close();</script></html>'
-        responseHTML = responseHTML.replace('%value%', JSON.stringify({
-            token: token
-        }));
-        res.status(200).send(responseHTML);
-    },
-);
-
-app.get("/api/auth/failure", (req, res) => {
-    res.send("Something went wrong!");
-});
-
-app.get("/api/protected", passport.authenticate("jwt", { session: false }), authUtils.isLoggedIn, (req, res) => {
-    res.send("SUCCESS!!!");
-});
-
-app.get("/api/logout", (req, res) => {
-    req.logOut();
-    res.send("Goodbye!");
+app.use('/api', apiRouter);
+app.use(express.static(__dirname + '/public'));
+app.use('/*', function(req, res){
+    res.sendfile(__dirname + '/public/index.html');
 });
 
 app.listen(3000, () => {
---
+++ src/routes.ts
@@ -0,0 +1,47 @@
+import { Response, Router } from 'express';
+import passport from "passport";
+import { authUtils } from "./auth/utils";
+
+export const apiRouter = Router();
+
+apiRouter.get("/auth/google", (req, res, next) => {
+        passport.authenticate("google", {
+            session: false,
+            scope: ["email", "profile"],
+// TODO add 'state' if necessary here
+            state: ""
+        })(req, res, next);
+    }
+);
+
+apiRouter.get("/auth/google/callback",
+    passport.authenticate(
+        "google",
+        {
+            session: false,
+            failureRedirect: "/auth/failure"
+        }
+    ),
+    (req, res: Response, next) => {
+        const token: string = authUtils.generateUserToken(req, res);
+        // This is html wrapper to be opened in separate window
+        let responseHTML = '<html><head><title>Main</title></head><body></body><script>res = %value%; window.opener.postMessage(res, "*");window.close();</script></html>'
+        responseHTML = responseHTML.replace('%value%', JSON.stringify({
+            token: token
+        }));
+        res.status(200).send(responseHTML);
+    },
+);
+
+apiRouter.get("/auth/failure", (req, res) => {
+    res.send("Something went wrong!");
+});
+
+apiRouter.get("/protected", passport.authenticate("jwt", { session: false }), authUtils.isLoggedIn, (req, res) => {
+    res.send("SUCCESS!!!");
+});
+
+apiRouter.get("/logout", (req, res) => {
+    req.logOut();
+    res.send("Goodbye!");
+});
